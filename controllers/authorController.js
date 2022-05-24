6c8f81152de318d31ca22f48dd52d60a3656621d
Lorenzo Ruiz Díaz <lorenzoruizdiaz2003@gmail.com>
2022-05-23 23:44:19 -0300

    modified controllers files to add try-catch blocks for possible errors

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,46 +1,68 @@
+const { ApiError } = require("../modules/error-handler");
 const authorService = require("../services/authorService");
 
-const getAllAuthors = async (req, res) => {
-    const allAuthors = await authorService.getAllAuthors();
-    res.status(200).send({ status: "OK", data: allAuthors });
+const getAllAuthors = async (req, res, next) => {
+    try {
+        const allAuthors = await authorService.getAllAuthors();
+        res.status(200).send({ status: "OK", data: allAuthors });
+    } catch (err) {
+        next(ApiError.internalServerError(err.message));
+    }
 }
 
 const getAuthor = async (req, res) => {
-    const id = req.params.authorId;
-    if (!id)
-        return;
-    
-    const author = await authorService.getAuthor(id);
-    res.status(200).send({ status: "OK", data: author });
+    try {
+        const id = req.params.authorId;
+        if (!id)
+            return;
+
+        const author = await authorService.getAuthor(id);
+        res.status(200).send({ status: "OK", data: author });
+    } catch (err) {
+        next(ApiError.internalServerError(err.message));
+    }
 }
 
 const createAuthor = async (req, res) => {
-    const { email, username, password } = req.body;
-    if (!email || !username || !password)
-        return;
+    try {
+        const { email, username, password } = req.body;
+        if (!email || !username || !password)
+            return;
 
-    const createdAuthor = await authorService.createAuthor(email, username, password);
-    res.status(201).send({ status: "OK", data: createdAuthor});
+        const createdAuthor = await authorService.createAuthor(email, username, password);
+        res.status(201).send({ status: "OK", data: createdAuthor });
+    } catch (err) {
+        next(ApiError.internalServerError(err.message));
+    }
 }
 
 const updateAuthorPassword = async (req, res) => {
-    const id = req.params.authorId;
-    const newPassword = req.body.newPassword;
+    try {
+        const id = req.params.authorId;
+        const newPassword = req.body.newPassword;
 
-    if (!id || !newPassword)
-        return;
+        if (!id || !newPassword)
+            return;
 
-    await authorService.updateAuthor(id, newPassword);
-    res.status(201).send({ status: "OK" });
+        await authorService.updateAuthor(id, newPassword);
+        res.status(201).send({ status: "OK" });
+    } catch (err) {
+        next(ApiError.internalServerError(err.message));
+    }
 }
 
 const deleteAuthor = async (req, res) => {
-    const id = req.params.authorId;
-    if (!id)
-        return;
-    
-    await authorService.deleteAuthor(id);
-    res.status(200).send({ status: "OK" });
+    try {
+        const id = req.params.authorId;
+        if (!id)
+            return;
+
+        await authorService.deleteAuthor(id);
+        res.status(200).send({ status: "OK" });
+    } catch (err) {
+        next(ApiError.internalServerError(err.message));
+    }
+
 }
 
 module.exports = {
--- controllers/newsController.js
+++ controllers/newsController.js
@@ -1,44 +1,65 @@
+const { ApiError } = require("../modules/error-handler");
 const newsService = require("../services/newsService");
 
 const getAllNewsArticles = async (req, res) => {
-    const allNewsArticles = await newsService.getAllNewsArticles();
-    res.status(200).send({ status: "OK", data: allNewsArticles });
+    try {
+        const allNewsArticles = await newsService.getAllNewsArticles();
+        res.status(200).send({ status: "OK", data: allNewsArticles });
+    } catch (err) {
+        next(ApiError.internalServerError(err.message));
+    }
 }
 
 const getNewsArticle = async (req, res) => {
-    const id = req.params.newsId;
-    if (!id)
-        return;
-    
-    const newsArticle = await newsService.getNewsArticle(id);
-    res.status(200).send({ status: "OK", data: newsArticle });
+    try {
+        const id = req.params.newsId;
+        if (!id)
+            return;
+
+        const newsArticle = await newsService.getNewsArticle(id);
+        res.status(200).send({ status: "OK", data: newsArticle });
+    } catch (err) {
+        next(ApiError.internalServerError(err.message));
+    }
 }
 
 const createNewsArticle = async (req, res) => {
-    const { headline, body, lead, author, category, tags, image } = req.body;
-    if (!headline || !body || !lead || !author || !category || !tags || !image)
-        return;
+    try {
+        const { headline, body, lead, author, category, tags, image } = req.body;
+        if (!headline || !body || !lead || !author || !category || !tags || !image)
+            return;
 
-    const createdNewsArticle = await newsService.createNewsArticle(req.body);
-    res.status(201).send({ status: "OK", data: createdNewsArticle });
+        const createdNewsArticle = await newsService.createNewsArticle(req.body);
+        res.status(201).send({ status: "OK", data: createdNewsArticle });
+    } catch (err) {
+        next(ApiError.internalServerError(err.message));
+    }
 }
 
 const updateNewsArticle = async (req, res) => {
-    const id = req.params.newsId;
-    if (!id)
-        return;
+    try {
+        const id = req.params.newsId;
+        if (!id)
+            return;
 
-    await newsService.updateNewsArticle(id, req.body);
-    res.status(201).send({ status: "OK" });
+        await newsService.updateNewsArticle(id, req.body);
+        res.status(201).send({ status: "OK" });
+    } catch (err) {
+        next(ApiError.internalServerError(err.message));
+    }
 }
 
 const deleteNewsArticle = async (req, res) => {
-    const id = req.params.newsId;
-    if (!id)
-        return;
+    try {
+        const id = req.params.newsId;
+        if (!id)
+            return;
 
-    await newsService.deleteNewsArticle(id);
-    res.status(200).send({ status: "OK" });
+        await newsService.deleteNewsArticle(id);
+        res.status(200).send({ status: "OK" });
+    } catch (err) {
+        next(ApiError.internalServerError(err.message));
+    }
 }
 
 module.exports = {
