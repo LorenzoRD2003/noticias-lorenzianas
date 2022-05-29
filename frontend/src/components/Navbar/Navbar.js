9f6cb237bd4c662ddd458441a87dc8eb1c48e12e
Lorenzo Ruiz Díaz <lorenzoruizdiaz2003@gmail.com>
2022-05-29 20:15:17 -0300

    Finished for today :)

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -1,10 +1,11 @@
 import React from "react";
+import { Link } from "react-router-dom"
 
-const NavbarTitle = props => <a class="navbar-brand" href={props.link}>{props.title}</a>
+const NavbarTitle = props => <Link class="navbar-brand" to={props.link}>{props.title}</Link>
 
 const NavbarItem = props => (
     <li class="nav-item">
-        <a class="nav-link" href={props.link}>{props.text}</a>
+        <Link class="nav-link" to={props.link}>{props.text}</Link>
     </li>
 );
 
